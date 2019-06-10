2b6268027ebcb2a8719f27236e629acfa1d1a8e4
unknown <enzohorquin@gmail.com>
2019-06-10 12:58:24 -0300

    Changes

---
+++ config/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    port: 5000,
+    secret:'secret',
+    nodemailer: {
+        host: process.env.CHALLENGE_MAIL_SERVICE || 'smtp.gmail.com',
+        auth: {
+            type: 'OAuth2',
+            clientId: '403056811268-ekh83h95jd6hci99mmbrfl4mtrjvghn0.apps.googleusercontent.com',
+            clientSecret: 'YoEqvqc9FJso6YRFwRoV4V47',
+            user: process.env.CHALLENGE_MAIL_USER || 'enzohorquin@gmail.com',
+            pass: process.env.CHALLENGE_MAIL_PASS || '38828881',
+            refreshToken:'1/p0ommkotoiFgNpN1rkQ3Rz2KASDPCpJ4DydppLWBzpY'
+        }
+    },
+    tokenTime: process.env.CHALLENGE_TOKEN_TIME || 30000,
+    resetTime: process.env.CHALLENGE_RESET_TIME || 30000
+};
